b6639b77e9cb921c42a85aa6024e6701e0868d55
devkeita <keita.mywork@gmail.com>
2021-11-02 20:15:25 +0900

    Login系のコンポーネントを作成

---
+++ src/components/common/RequiredNotLogin.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { accessTokenSelectors } from "../../states/accessToken";
+import { Loading } from "../loading/loading";
+
+type Props = {
+  children?: React.ReactNode;
+  redirectTo?: string;
+};
+
+export const RequiredNotLogin: React.FC<Props> = ({
+  children,
+  redirectTo = "/",
+}) => {
+  const [loading, setLoading] = useState(true);
+  const accessToken = accessTokenSelectors.useAccessToken();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.isReady) {
+      setLoading(false);
+    }
+  }, [router]);
+
+  if (loading) return <Loading />;
+
+  if (accessToken) {
+    router.push(redirectTo);
+    return null;
+  }
+
+  return <>{children}</>;
+};
