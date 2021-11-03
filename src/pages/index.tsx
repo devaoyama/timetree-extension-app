a10c9df7bc354cbf3a9cc5eee6092e8bdc6fb451
devkeita <keita.mywork@gmail.com>
2021-11-03 22:03:28 +0900

    ページを修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,46 @@
-import React from "react";
-import { Box, Button, Flex, Heading, Stack } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { Box, Flex, Heading, Stack } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
+import {
+  accessTokenActions,
+  accessTokenSelectors,
+} from "../states/accessToken";
+import { RequiredLogin } from "../components/common/RequiredLogin";
 
 const Home: NextPage = () => {
   const router = useRouter();
+  const accessToken = accessTokenSelectors.useAccessToken();
+  const requestAccessToken = accessTokenActions.useRequestAccessToken();
 
-  const onClickButton = () => {
-    router.push(
-      `https://timetr.ee/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_TIMETREE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_TIMETREE_REDIRECT_URI}&response_type=code`
-    );
-  };
+  useEffect(() => {
+    if (!router.query.code) return;
+    requestAccessToken(router.query.code as string);
+  }, [requestAccessToken, router.query.code]);
 
   return (
-    <Flex
-      flexDirection="column"
-      width="100wh"
-      height="100vh"
-      backgroundColor="gray.200"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Stack
-        flexDir="column"
-        mb="3"
+    <RequiredLogin>
+      <Flex
+        flexDirection="column"
+        width="100wh"
+        height="100vh"
+        backgroundColor="gray.200"
         justifyContent="center"
         alignItems="center"
-        width="full"
       >
-        <Heading color="teal.400">Welcome</Heading>
-        <Box minW={{ base: "90%", md: "468px" }}>
-          <Button
-            borderRadius={0}
-            type="submit"
-            variant="solid"
-            colorScheme="teal"
-            width="full"
-            onClick={onClickButton}
-          >
-            ログイン
-          </Button>
-        </Box>
-      </Stack>
-      <Box>TimeTreeにログインします</Box>
-    </Flex>
+        <Stack
+          flexDir="column"
+          mb="3"
+          justifyContent="center"
+          alignItems="center"
+          width="full"
+        >
+          <Heading color="teal.400">Welcome</Heading>
+        </Stack>
+        <Box>ログイン中</Box>
+        <Box>{accessToken}</Box>
+      </Flex>
+    </RequiredLogin>
   );
 };
 
--- src/pages/redirect.tsx
+++ src/pages/redirect.tsx
@@ -1,44 +1,23 @@
 import React, { useEffect } from "react";
-import { Box, Flex, Heading, Stack } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import {
-  accessTokenActions,
-  accessTokenSelectors,
-} from "../states/accessToken";
+import { accessTokenActions } from "../states/accessToken";
+import { Loading } from "../components/loading/loading";
 
-const Home: NextPage = () => {
+const Redirect: NextPage = () => {
   const router = useRouter();
-  const accessToken = accessTokenSelectors.useAccessToken();
   const requestAccessToken = accessTokenActions.useRequestAccessToken();
 
   useEffect(() => {
-    if (!router.query.code) return;
+    if (!router.query.code) {
+      router.push("/login");
+      return;
+    }
     requestAccessToken(router.query.code as string);
-  }, [requestAccessToken, router.query.code]);
+    router.push("/");
+  }, [requestAccessToken, router, router.query.code]);
 
-  return (
-    <Flex
-      flexDirection="column"
-      width="100wh"
-      height="100vh"
-      backgroundColor="gray.200"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Stack
-        flexDir="column"
-        mb="3"
-        justifyContent="center"
-        alignItems="center"
-        width="full"
-      >
-        <Heading color="teal.400">Welcome</Heading>
-      </Stack>
-      <Box>アクセストークンゲット</Box>
-      <Box>{accessToken}</Box>
-    </Flex>
-  );
+  return <Loading />;
 };
 
-export default Home;
+export default Redirect;
