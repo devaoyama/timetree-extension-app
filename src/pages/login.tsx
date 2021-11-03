defa5acc8a1929e14908b3291853d57df7c6f62f
devkeita <keita.mywork@gmail.com>
2021-11-03 21:58:41 +0900

    loginページを作成

---
+++ src/pages/login.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Box, Button, Flex, Heading, Stack } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { RequiredNotLogin } from "../components/common/RequiredNotLogin";
+
+const Login: NextPage = () => {
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push(
+      `https://timetr.ee/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_TIMETREE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_TIMETREE_REDIRECT_URI}&response_type=code`
+    );
+  };
+
+  return (
+    <RequiredNotLogin>
+      <Flex
+        flexDirection="column"
+        width="100wh"
+        height="100vh"
+        backgroundColor="gray.200"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Stack
+          flexDir="column"
+          mb="3"
+          justifyContent="center"
+          alignItems="center"
+          width="full"
+        >
+          <Heading color="teal.400">Welcome</Heading>
+          <Box minW={{ base: "90%", md: "468px" }}>
+            <Button
+              borderRadius={0}
+              type="submit"
+              variant="solid"
+              colorScheme="teal"
+              width="full"
+              onClick={onClickButton}
+            >
+              ログイン
+            </Button>
+          </Box>
+        </Stack>
+        <Box>TimeTreeにログインします</Box>
+      </Flex>
+    </RequiredNotLogin>
+  );
+};
+
+export default Login;
