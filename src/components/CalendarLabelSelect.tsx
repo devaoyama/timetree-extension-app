7ae7e0df8f7af25cd086bc223f0159d8da935ade
devkeita <keita.mywork@gmail.com>
2021-11-10 22:24:56 +0900

    CalendarLabelSelectを作成

---
+++ src/components/CalendarLabelSelect.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { Flex, Select, Text } from "@chakra-ui/react";
+import { labelSelectors } from "../states/labels";
+type Props = {
+  calendarId: string;
+  calendarName: string;
+  onChange: (labelId: string) => void;
+};
+
+export const CalendarLabelSelect: React.FC<Props> = ({
+  calendarId,
+  calendarName,
+  onChange,
+}) => {
+  const [color, setColor] = useState<string | undefined>();
+  const { data } = labelSelectors.useLabels(calendarId);
+
+  return (
+    <Flex align="center" pl={3}>
+      <Text width="30%">{calendarName}</Text>
+      <Select
+        color={color}
+        focusBorderColor={color}
+        placeholder="ラベルを選択してください"
+        onChange={(e) => {
+          setColor(data?.find((label) => label.id === e.target.value)?.color);
+          onChange(e.target.value);
+        }}
+      >
+        {data?.map((label) => (
+          <option key={label.id} value={label.id}>
+            {label.name}
+          </option>
+        ))}
+      </Select>
+    </Flex>
+  );
+};
