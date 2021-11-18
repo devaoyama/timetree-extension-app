fab907e51ccf22e0879190de718cdd4b104a9353
devkeita <keita.mywork@gmail.com>
2021-11-18 22:55:12 +0900

    viewportを削除

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,10 +8,6 @@ function MyDocument() {
         <meta name="theme-color" content="#38A169" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-        />
       </Head>
       <body>
         <Main />
